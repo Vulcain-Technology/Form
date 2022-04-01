51ed69654c25191ac1f9ebfd6dc1640bc3203a76
Gabriel-Vanroy <contact@gabriel-web-studio.fr>
2022-04-01 20:03:40 +0200

    Update input component

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -2,16 +2,17 @@
 
 namespace VulcainTechnology\Form\Console;
 
+use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 
-class InstallCommand
+class InstallCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected string $signature = 'vulcain-form:install
+    protected $signature = 'vulcain-form:install
                                    {--base : Just install nodeJs packages without update CSS/JS files (use this only if you have already install TailwindCSS and AlpineJS previously)}';
 
     /**
@@ -19,7 +20,7 @@ class InstallCommand
      *
      * @var string
      */
-    protected string $description = 'Install the Vulcain Technology - Form resources';
+    protected $description = 'Install the Vulcain Technology Form resources';
 
 
     public function handle()
@@ -43,6 +44,9 @@ class InstallCommand
         // Components...
         (new Filesystem)->ensureDirectoryExists(app_path('View/Components'));
         (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/App/View/Components', app_path('View/Components'));
+
+        $this->info('Vulcain form scaffolding installed successfully.');
+        $this->comment('Please execute the "npm install && npm run dev" command to build your assets.');
     }
 
     /**
